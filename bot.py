295e1b692a0fe4f3d163b96e52360e41be644b69
Andrii <andrey.aksonov@gmail.com>
2025-07-05 00:09:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,12 @@
+import os
 import json
 import telebot
 from ai_deepseek import AI
 from telebot import types
 import random
 
-
-file = open("TOKEN.json", "r",)
-TOKEN = json.load(file)["TOKEN"]
-file.close()
+# Отримуємо токен із змінної оточення
+TOKEN = os.environ['TELEGRAM_TOKEN']
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -18,12 +17,10 @@ with open("settings.json", 'r') as f:
 me = bot.get_me()
 bot_username = '@' + me.username
 
-
 @bot.message_handler(commands=["start"])
 def start(msg: types.Message):
     bot.send_message(msg.chat.id, "Бот працює, ось список команд:\n\
 /clear_message_hist - Очистити історію повідомлень")
-
 
 @bot.message_handler(commands=["clear_message_hist"])
 def clear_msg_hist(msg: types.Message):
@@ -34,14 +31,12 @@ def clear_msg_hist(msg: types.Message):
         bot.send_message(msg.chat.id, "Щось пішло не так")
         print(e)
 
-
 @bot.message_handler(content_types=["text"])
 def txt_handler(msg: types.Message):
     if bot_username in [word for word in msg.text.split(' ')]:
         bot.send_message(msg.chat.id, f"{AI.send_request(msg)}")
-
     elif len([1 for word in msg.text.split(' ') if word.strip('?!@#%^&*()').lower() in ["даша", "дашуня", "дашка"]]) > 0:
         bot.send_message(msg.chat.id, f"{AI.send_request(msg)}")
-
     elif random.randrange(0, 100) <= 20:
         bot.send_message(msg.chat.id, f"{AI.send_request(msg)}")
+
